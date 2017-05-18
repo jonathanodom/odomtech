55d5cc653deba903897540d9c9b282977b43003e
jonathanodom <jonathan@discipleshipwebdesigns.com>
2017-05-17 21:12:43 -0500

    Remove unfinished post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -31,17 +31,7 @@
       <div class="row">
 
         <div class="col-sm-7 blog-main">
-          <div class="blog-post">
-
-            <h2 class="blog-post-title"><a href="/blog/God's Love">God's Love</a></h2>
-            <p class="blog-post-meta">March 9, 2017 by <a href="#">Jonathan</a></p>
-
-            
-            <h3>Romans 8:35-39</h3>
-            <p>As I was trying to prepare something for my first Devo blog post I kept coming to this passage in Romans 8:31-39 specifically verse 35 "Can anything ever seperate us from Christ's love". So I continued to reading and searching for the <em>"why"</em>. <em>Why do I keep coming back to Romans chapter 8? Why is the important to me?</em> Then it hit me.... the whole point to a devotion is to link back to an emotional part of my life that someone might need to hear. So lets take a step back a few years (7-9 years ago). I was on a path of destruction letting everyday lifes struggles fill my heart with doubt. I struggled with depression and anger management issues as a teenager from when I was about 16 thru my early 20s. Once I was able to leave <em>"mom & dads"</em> I quit going to church and started directing my own path of life.  </p>
-            
-            
-          </div><!-- /.blog-post -->
+          
           <div class="blog-post">
             <h2 class="blog-post-title"><a href="/blog/First Things First">First Things First</a></h2>
             <p class="blog-post-meta">March 5, 2017 by <a href="#">Jonathan</a></p>
@@ -61,7 +51,7 @@
               <br>My current projects are an update to the Bible Search app and a christmas light show made with a Raspberry Pi. 
             </p>
             <h4>My Goals</h4>
-            <p>My main goal for this blog is to grow in my Faith and strengthen my relationship with God by sharing and studying His Word with you(the reader) by posting a weekly reflection/devotion of my current study of God's Word. Expect the first devo be on Thursday March 9<sup>th</sup>. I'm going to talk about new Christian music releases and music that has meaning in my life, new electronics for DIY projects and my DIY projects I am working on, my journey of fatherhood with twins, and Computer games, and the best of all a Verse of the Day and a Song to get you through the working hours.  </p>
+            <p>My main goal for this blog is to grow in my Faith and strengthen my relationship with God by sharing and studying His Word with you(the reader) by posting a weekly reflection/devotion of my current study of God's Word. I'm going to talk about new Christian music releases and music that has meaning in my life, new electronics for DIY projects and my DIY projects I am working on, my journey of fatherhood with twins, and Computer games, and the best of all a Verse of the Day and a Song to get you through the working hours.  </p>
             <h3></h3>
             
           </div><!-- /.blog-post -->
